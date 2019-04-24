7ddc32ec761d19e7d58b5bb98d51f57f0c6f51b1
Chris Brandt <chris.brandt@renesas.com>
2019-04-24 09:10:35 -0500

    setup_env.sh: Detect toolchain and sysroot
    Move the detection of the toolchain and sysroot from the Makefiles to the
    setup_env.sh file. This makes the Makefiles easier to write, and also
    helps when moving from different versions of Buildroot because the location
    of the toolchain has changed.

--- setup_env.sh
+++ setup_env.sh
@@ -28,17 +28,31 @@ if [ -e $OUTDIR/br_version.txt ] ; then
 fi
 export BUILDROOT_DIR=$OUTDIR/buildroot-$BR_VERSION
 
-# Toolchain directory (for u-boot and kernel)
-if [ -e $OUTDIR/buildroot-$BR_VERSION/output/host ] ; then
 
-  export TOOLCHAIN_DIR=$OUTDIR/host/usr
+# Define the base path of the toolchain
+if [ -e $BUILDROOT_DIR/output/host/bin ] ; then
+  # Buildroot versions after 2018, output/host/bin contains host apps for building
+  export TC_PATH=$BUILDROOT_DIR/output/host
+else
+  # Buildroot versions before 2018, output/host/usr/bin contains host apps for building
+  export TC_PATH=$BUILDROOT_DIR/output/host/usr
+fi
+export TOOLCHAIN_DIR=$TC_PATH
 
-  # set toolchain prefix and add to path
-  cd $OUTDIR/buildroot-$BR_VERSION/output/host/usr/bin
-  export CROSS_COMPILE=`ls *gnueabi*-gcc | sed 's/gcc//'`
-  export PATH=`pwd`:$PATH
-  cd -
-  export ARCH=arm
+# Set toolchain prefix and add to path
+cd $TC_PATH/bin
+export CROSS_COMPILE=`ls *abi*-gcc | sed 's/gcc//'`
+export PATH=`pwd`:$PATH
+cd -
+export ARCH=arm
+
+# Find the location of the sysroot where all the libraries and header files are
+export SYSROOT_DIR=`find $TC_PATH -name sysroot`
+if [ "$SYSROOT_DIR" == "" ] ; then
+  echo "==========================================="
+  echo "  ERROR: Cannot find sysroot"
+  echo "         Toolchain path = $TC_PATH"
+  echo "==========================================="
 fi
 
 
@@ -47,19 +61,19 @@ fi
 # -------------------------------------------------
 # Uncomment the prompt you want
 
-# Change prompt to (rza1_bsp)
-#PS1="(rza1_bsp)$ "
+# Change prompt to (rza_bsp)
+#PS1="(rza_bsp)$ "
 
-# Change prompt to (rza1_bsp) with RED text
-#PS1="\[\e[1;31m\](rza1_bsp)$\[\e[00m\] "
+# Change prompt to (rza_bsp) with RED text
+#PS1="\[\e[1;31m\](rza_bsp)$\[\e[00m\] "
 
-# Change prompt to (rza1_bsp) with RED text
+# Change prompt to (rza_bsp) with RED text
 # with current directory printed out on the line above
-#PS1="dir: \w\n\[\e[1;31m\](rza1_bsp)$\[\e[00m\] "
+#PS1="dir: \w\n\[\e[1;31m\](rza_bsp)$\[\e[00m\] "
 
-# Change prompt to (rza1_bsp) with RED text
+# Change prompt to (rza_bsp) with RED text
 # with current directory printed out on the line above in ORANGE text
-PS1="\[\e[33m\]dir: \w\n\[\e[1;31m\](rza1_bsp)$\[\e[00m\] "
+PS1="\[\e[33m\]dir: \w\n\[\e[1;31m\](rza_bsp)$\[\e[00m\] "
 
 
 echo "Build Environment set"
